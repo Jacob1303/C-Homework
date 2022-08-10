a062f656625f44a2bfd3ddf8a57ae00bc3a2813c
yakov kuzin <kuzin_js@mail.ru>
2022-08-10 14:27:08 +0300

    Homework3.Done

---
+++ Homework3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введи целое число, мой маленький математик, и случится чудо ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % 2 == 0)
+{
+    Console.WriteLine("Твоё число - чётное");
+}
+else
+{
+    Console.WriteLine("Твоё число - нечётное");
+}
