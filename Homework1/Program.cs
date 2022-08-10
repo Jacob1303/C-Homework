ab3b1f6d1f89605b36d4e07312bb1f05acbbad0e
yakov kuzin <kuzin_js@mail.ru>
2022-08-10 13:19:09 +0300

    Homework1.Done

---
+++ Homework1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите 2 целых числа");
+Console.Write("Число 1 - ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Число 2 - ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2)
+{
+    Console.Write("Числа равны");
+}
+
+if (number1 > number2)
+ {
+    Console.Write("Большим значением является ");
+    Console.WriteLine(number1);
+    Console.Write("Меньшим значением является ");
+    Console.WriteLine(number2);
+}
+
+if (number1 < number2)
+{
+    Console.Write("Большим значением является ");
+    Console.WriteLine(number2);
+    Console.Write("Меньшим значением является ");
+    Console.WriteLine(number1);
+}
+    
