6f5692d314c917899b3e03c4b84305da578e7833
yakov kuzin <kuzin_js@mail.ru>
2022-08-10 14:50:56 +0300

    Homework4.Done

---
+++ Homework4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("И снова здравствуй! Введи целое положительное число больше 1 и ты получишь...");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int number2 = 2;
+
+if (number1 > 1) 
+{
+    while (number2 <= number1)
+    {
+        Console.Write(number2 + " ");
+       
+        number2 = number2 + 2;
+    }  
+    Console.WriteLine("Это все чётные числа до введённого тобой. Пользуйся!");      
+}
+else
+{
+    Console.WriteLine("Прочти ещё раз условие и введи корректное число");
+}
